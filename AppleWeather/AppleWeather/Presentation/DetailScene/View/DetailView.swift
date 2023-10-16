8d0edfdc6647cb725983a6f36e9d75c841aa5982
ahra1221 <lizze011221@gmail.com>
2023-10-16 23:15:00 +0900

    [Setting] #1 - 프로젝트 세팅

---
+++ AppleWeather/AppleWeather/Presentation/DetailScene/View/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  AppleWeather
+//
+//  Created by 고아라 on 2023/10/16.
+//
+
+import Foundation
---
+++ AppleWeather/AppleWeather/Presentation/MainScene/View/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  AppleWeather
+//
+//  Created by 고아라 on 2023/10/16.
+//
+
+import Foundation
