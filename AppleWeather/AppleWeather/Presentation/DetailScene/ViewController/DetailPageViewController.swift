5a1c57b28ddad136dfad85a203f2730acde6fe5e
ahra1221 <lizze011221@gmail.com>
2023-10-24 21:10:11 +0900

    [Feat] #1 - UIPageControl 구현

--- AppleWeather/AppleWeather/Presentation/DetailScene/ViewController/DetailPageViewController.swift
+++ AppleWeather/AppleWeather/Presentation/DetailScene/ViewController/DetailPageViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import SnapKit
+
 final class DetailPageViewController: UIPageViewController {
     
     // MARK: - Properties
@@ -15,6 +17,44 @@ final class DetailPageViewController: UIPageViewController {
     private var pages = [UIViewController]()
     private var weatherEntity : [WeatherEntity] = WeatherEntity.mainEntityDummy()
     
+    // MARK: - UI Components
+    
+    private let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPageIndicatorTintColor = .WeatherWhite
+        pageControl.pageIndicatorTintColor = .WeatherWhite.withAlphaComponent(0.3)
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        return pageControl
+    }()
+    
+    private let toolbar: UIToolbar = {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        toolbar.backgroundColor = .clear
+        toolbar.tintColor = .WeatherWhite
+        toolbar.barTintColor = .clear
+        toolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        return toolbar
+    }()
+    
+    private let underlineView: UIView = {
+        let line = UIView()
+        line.backgroundColor = .WeatherGray3
+        return line
+    }()
+    
+    private lazy var mapButton: UIButton = {
+        let button = UIButton()
+        button.setImage(ImageLiterals.Detail.ic_map, for: .normal)
+        return button
+    }()
+    
+    private lazy var listButton: UIButton = {
+        let button = UIButton()
+        button.setImage(ImageLiterals.Detail.ic_list, for: .normal)
+        return button
+    }()
+    
     // MARK: - Life Cycles
     
     override func viewDidLoad() {
@@ -22,6 +62,10 @@ final class DetailPageViewController: UIPageViewController {
         
         setPage()
         setUI()
+        setHierarchy()
+        setLayout()
+        setToolBar()
+        setAddTarget()
     }
 }
 
@@ -34,11 +78,57 @@ extension DetailPageViewController {
             detailVC.setDataBind(index: i)
             pages.append(detailVC)
         }
+        self.pageControl.numberOfPages = weatherEntity.count
+        self.pageControl.setIndicatorImage(ImageLiterals.Detail.ic_nav, forPage: 0)
+        self.pageControl.currentPage = initialPage
     }
     
     func setUI() {
         self.dataSource = self
+        self.delegate = self
         self.setViewControllers([pages[initialPage]], direction: .forward, animated: true)
+    }
+    
+    func setHierarchy() {
+        view.addSubviews(toolbar, underlineView)
+    }
+    
+    func setLayout() {
+        toolbar.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(62)
+        }
+        
+        underlineView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(toolbar.snp.top)
+            $0.height.equalTo(1)
+        }
+    }
+    
+    func setToolBar() {
+        let leftButton = UIBarButtonItem(customView: mapButton)
+        let rightButton = UIBarButtonItem(customView: listButton)
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let pageControl = UIBarButtonItem(customView: pageControl)
+        toolbar.setItems([leftButton, flexibleSpace, pageControl, flexibleSpace, rightButton], animated: true)
+    }
+    
+    func setAddTarget() {
+        mapButton.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
+        listButton.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
+    }
+    
+    @objc
+    func tapButton(_ sender: UIButton) {
+        switch sender {
+        case mapButton:
+            print("🗺️")
+        case listButton:
+            self.navigationController?.popViewController(animated: true)
+        default:
+            break
+        }
     }
 }
 
@@ -55,5 +145,15 @@ extension DetailPageViewController: UIPageViewControllerDataSource {
         
         guard currentIndex < (pages.count - 1) else { return nil }
         return pages[currentIndex + 1]
+    }
+}
+
+extension DetailPageViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard completed else { return }
+        if let currentViewController = viewControllers?.first,
+           let currentIndex = pages.firstIndex(of: currentViewController) {
+            pageControl.currentPage = currentIndex
+        }
     }
 }
