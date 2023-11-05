e67d46a608d91649cb40f7e8f4d4d58d1d8e16c3
ahra1221 <lizze011221@gmail.com>
2023-11-05 22:29:48 +0900

    [Feat] #3 - 스크롤 시 stickyheader 구현

--- AppleWeather/AppleWeather/Presentation/DetailScene/ViewController/DetailViewController.swift
+++ AppleWeather/AppleWeather/Presentation/DetailScene/ViewController/DetailViewController.swift
@@ -22,13 +22,37 @@ class DetailViewController: UIViewController {
     
     private let weatherEntity : [WeatherEntity] = WeatherEntity.mainEntityDummy()
     
+    private enum HeaderHeight {
+        static let statusBarHeight = UIApplication.shared.statusBarFrame.height + 6
+        static let stickyHeaderHeightMin = 80.0
+        static let stickyHeaderHeightMax = 220.0
+        static var stickyHeaderHeightMaxWithoutStatusBar: Double {
+            stickyHeaderHeightMax - statusBarHeight
+        }
+    }
+    
+    private var lowestTemp: Int = 0
+    private var highestTemp: Int = 0
+    
     // MARK: - UI Components
     
     private let backgroundImage: UIImageView = UIImageView(image: ImageLiterals.Detail.img_bigBackground)
-    private let scrollView = UIScrollView()
-    private let contentView = UIView()
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     private let detailTitleView = DetailTitleView()
+    private let detailSmallTitleView = DetailSmallTitleView()
+    
     private let detailCollectionView = DetailCollectionView()
     lazy var collectionView = detailCollectionView.DetailCollectionView
     private let detailTableView = DetailTableView()
@@ -39,18 +63,22 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
         setHierarchy()
         setLayout()
         setDelegate()
     }
-    
 }
 
 // MARK: - Extensions
 
 extension DetailViewController {
+    func setUI() {
+        detailSmallTitleView.alpha = 0
+    }
+    
     func setHierarchy() {
-        view.addSubviews(backgroundImage, scrollView)
+        view.addSubviews(backgroundImage, scrollView, detailSmallTitleView)
         scrollView.addSubview(contentView)
         contentView.addSubviews(detailTitleView, detailCollectionView, detailTableView)
     }
@@ -61,18 +89,26 @@ extension DetailViewController {
         }
         
         scrollView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
         }
         
         contentView.snp.makeConstraints {
-            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.edges.equalToSuperview()
             $0.width.equalTo(scrollView.snp.width)
             $0.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
         }
         
         detailTitleView.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(34)
+            $0.top.equalToSuperview().inset(34)
             $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(HeaderHeight.stickyHeaderHeightMax)
+        }
+        
+        detailSmallTitleView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(HeaderHeight.stickyHeaderHeightMin)
         }
         
         detailCollectionView.snp.makeConstraints {
@@ -94,10 +130,12 @@ extension DetailViewController {
         collectionView.dataSource = self
         tableView.delegate = self
         tableView.dataSource = self
+        scrollView.delegate = self
     }
     
     func setDataBind(index: Int) {
         detailTitleView.setDataBind(model: weatherEntity[index])
+        detailSmallTitleView.setDataBind(model: weatherEntity[index])
         self.indexPage = index
     }
 }
@@ -142,5 +180,28 @@ extension DetailViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return weatherEntity[indexPage].dayWeather.count
+    }
+}
+
+extension DetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let maxOffset: CGFloat = HeaderHeight.stickyHeaderHeightMaxWithoutStatusBar
+        
+        var remainingTopSpacingRatio: CGFloat
+        if offsetY >= maxOffset {
+            remainingTopSpacingRatio = 0
+            scrollView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide).offset(HeaderHeight.stickyHeaderHeightMin + 20)
+            }
+            self.view.layoutIfNeeded()
+        } else {
+            remainingTopSpacingRatio = 1 - (offsetY / maxOffset)
+            scrollView.snp.updateConstraints {
+                $0.top.equalTo(view.safeAreaLayoutGuide)
+            }
+        }
+        detailTitleView.alpha = remainingTopSpacingRatio
+        detailSmallTitleView.alpha = 1 - remainingTopSpacingRatio
     }
 }
